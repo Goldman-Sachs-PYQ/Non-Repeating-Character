b861ad807c33972bbb8eb156879747292e8e4d0e
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-08-28 17:23:45 +0530

    Update Non Repeating Character.cpp

--- Non Repeating Character.cpp
+++ Non Repeating Character.cpp
@@ -15,33 +15,25 @@ class Solution
     // or not
     for (int i = 0; i<str.length(); i++)
     {
-        // If the current characters
-        // is not found then insert
-        // current characters with
-        // frequency 1
-        if (M.find(str[i]) == M.end())
-        {
-            M.insert(make_pair(str[i], 1));
-        }
- 
-        // Else update the frequency
-        else if(M.find(str[i]) != M.end())
-        {
-            M[str[i]]++;
-        }
+        M[str[i]]++;
     }
     
     // Traverse the map to print the
     // frequency
-    for (auto it : M) 
+    int flag=0;
+    for (int i = 0; i<str.length(); i++) 
     {
-        if(it.second==1)
+        if(M[str[i]]==1)
         {
-            ans=it.first;
+            ans=str[i];
+            flag=1;
             break;
         }
     }
-    
+    if(flag==0)
+    {
+        ans='$';
+    }
     return ans;
     }
 
